d2b35bfd8b7fce266ed49576b796b35b2c4c4a55
Ben Dalton <benadalton@gmail.com>
2024-01-08 22:28:38 -0500

    vim: check for changes made outside of vim

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -1,42 +1,42 @@
-vim.cmd('autocmd!')
+vim.cmd("autocmd!")
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 
 vim.wo.number = true
 
 vim.opt.ai = true -- auto indent
 vim.opt.autoindent = true
 vim.opt.autoread = true
-vim.opt.backspace = 'start,eol,indent'
+vim.opt.backspace = "start,eol,indent"
 vim.opt.backup = false
-vim.opt.backupskip = '/tmp/*,/private/tmp/*'
+vim.opt.backupskip = "/tmp/*,/private/tmp/*"
 vim.opt.breakindent = true
 vim.opt.cmdheight = 1
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 vim.opt.laststatus = 2
-vim.opt.path:append { '**' } -- finding files - search in subfolders
+vim.opt.path:append({ "**" }) -- finding files - search in subfolders
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
-vim.opt.shell = 'zsh'
+vim.opt.shell = "zsh"
 vim.opt.showcmd = true
 vim.opt.si = true -- smart indent
-vim.opt.signcolumn = 'yes:1'
+vim.opt.signcolumn = "yes:1"
 vim.opt.smartcase = true
 vim.opt.smarttab = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.textwidth = 0
 vim.opt.title = true
-vim.opt.wildignore:append { '*/node_modules/*' }
+vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.wrap = true -- wrap lines
 vim.opt.wrapmargin = 5
 vim.opt.writebackup = false
@@ -46,23 +46,30 @@ vim.opt.swapfile = false
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.winblend = 0
-vim.opt.wildoptions = 'pum'
+vim.opt.wildoptions = "pum"
 vim.opt.pumblend = 5
-vim.opt.background = 'dark'
+vim.opt.background = "dark"
+
+-- autoread changes outside of vim
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = { "*" },
+})
 
 -- Turn off paste mode when leaving insert
-vim.api.nvim_create_autocmd('InsertLeave', {
-  pattern = '*',
-  command = 'set nopaste'
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*",
+	command = "set nopaste",
 })
 
 -- Use clipboard for mac
-vim.opt.clipboard:append { 'unnamedplus' }
+vim.opt.clipboard:append({ "unnamedplus" })
 
 -- set text width for markdown
 vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = { "*.txt", "*.md" }, command = "setlocal textwidth=80" }
+	{ "BufRead", "BufNewFile" },
+	{ pattern = { "*.txt", "*.md" }, command = "setlocal textwidth=80" }
 )
 
 -- vim.api.nvim_create_autocmd(
@@ -70,4 +77,4 @@ vim.api.nvim_create_autocmd(
 --   { pattern = { "*.tsx", "*.ts" }, command = "OrganizeImports" }
 -- )
 
-vim.opt.formatoptions:append { ':rcqt' }
+vim.opt.formatoptions:append({ ":rcqt" })
