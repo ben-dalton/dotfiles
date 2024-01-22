43bb0ddd1b211c4ac35800ab1581ffe153720efe
Ben Dalton <benadalton@gmail.com>
2024-01-22 17:29:47 -0500

    Adds grep args

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,73 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    },
     config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+      require("telescope").load_extension("live_grep_args")
+
+      local telescope = require("telescope")
+      local lga_actions = require("telescope-live-grep-args.actions")
+      local lga_shortcuts = require("telescope-live-grep-args.shortcuts")
+      local builtin = require("telescope.builtin")
+
+      vim.keymap.set(
+        "n",
+        "<leader>fg",
+        ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"
+      )
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      -- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+      vim.keymap.set("n", "<leader>gc", lga_shortcuts.grep_word_under_cursor)
+
+      telescope.setup({
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            -- define mappings, e.g.
+            mappings = {
+              -- extend mappings
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+              },
+            },
+            -- ... also accepts theme settings, for example:
+            -- theme = "dropdown", -- use dropdown theme
+            -- theme = { }, -- use own theme spec
+            -- layout_config = { mirror=true }, -- mirror preview pane
+          },
+        },
+      })
+    end,
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       -- This is your opts table
-      require("telescope").setup {
+      require("telescope").setup({
+        -- pickers = {
+        --   live_grep = {
+        --     additional_args = function(prompt)
+        --       return { "--hidden" }
+        --     end,
+        --   },
+        -- },
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
+            require("telescope.themes").get_dropdown({
               -- even more opts
-            }
+            }),
 
             -- pseudo code / specification for writing custom displays, like the one
             -- for "codeactions"
@@ -35,12 +82,12 @@ return {
             --      do the following
             --   codeactions = false,
             -- }
-          }
-        }
-      }
+          },
+        },
+      })
       -- To get ui-select loaded and working with telescope, you need to call
       -- load_extension, somewhere after setup function:
       require("telescope").load_extension("ui-select")
-    end
-  }
+    end,
+  },
 }
