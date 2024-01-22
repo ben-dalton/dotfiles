1f425328e3015a8cf3e55460917b87821dd151d9
Ben Dalton <benadalton@gmail.com>
2024-01-22 17:27:59 -0500

    Adds renamer

--- .config/nvim/lua/plugins/other.lua
+++ .config/nvim/lua/plugins/other.lua
@@ -1,4 +1,30 @@
 return {
+	{
+		"filipdutescu/renamer.nvim",
+		branch = "master",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("renamer").setup()
+			vim.api.nvim_set_keymap(
+				"i",
+				"<F2>",
+				'<cmd>lua require("renamer").rename()<cr>',
+				{ noremap = true, silent = true }
+			)
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>rn",
+				'<cmd>lua require("renamer").rename()<cr>',
+				{ noremap = true, silent = true }
+			)
+			vim.api.nvim_set_keymap(
+				"v",
+				"<leader>rn",
+				'<cmd>lua require("renamer").rename()<cr>',
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
 	{ "wesQ3/vim-windowswap" },
 	{
 		"kylechui/nvim-surround",
