73f5406046272924f77ff76a4102b7fabfd25511
Ben Dalton <benadalton@gmail.com>
2024-02-14 17:36:49 -0500

    use git signs for current line blame

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -33,7 +33,7 @@ return {
 					follow_files = true,
 				},
 				attach_to_untracked = true,
-				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 				current_line_blame_opts = {
 					virt_text = true,
 					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
@@ -59,5 +59,6 @@ return {
 				},
 			})
 		end,
+		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>"),
 	},
 }
